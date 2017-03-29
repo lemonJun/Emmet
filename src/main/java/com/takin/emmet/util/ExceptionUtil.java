d00e455538404e4ffd448ef2f4fc1e061dc33fc6
lemonJun <506526593@qq.com>
2017-03-29 12:30:15 +0800

    up

---
+++ src/main/java/com/takin/emmet/util/ExceptionUtil.java
@@ -0,0 +1,20 @@
+package com.takin.emmet.util;
+
+public class ExceptionUtil {
+
+    public static String exceptionSimpleDesc(final Throwable e) {
+        StringBuffer sb = new StringBuffer();
+        if (e != null) {
+            sb.append(e.toString());
+
+            StackTraceElement[] stackTrace = e.getStackTrace();
+            if (stackTrace != null && stackTrace.length > 0) {
+                StackTraceElement elment = stackTrace[0];
+                sb.append(", ");
+                sb.append(elment.toString());
+            }
+        }
+
+        return sb.toString();
+    }
+}
