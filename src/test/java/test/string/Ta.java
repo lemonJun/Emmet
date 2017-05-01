af8fa2c8364b3328eded4bb9f30afdca190ded16
lemonJun <506526593@qq.com>
2017-05-01 14:01:11 +0800

    add csdn download

---
+++ src/test/java/test/string/Ta.java
@@ -0,0 +1,17 @@
+package test.string;
+
+public class Ta {
+
+    private static String sort(String raw) {
+        String str = raw.replace("（", "").replace("）", "").replace("一", "1").replace("二", "2");
+        str.replace("三", "3").replace("四", "4").replace("五", "5").replace("六", "6");
+        str.replace("七", "7").replace("八", "8").replace("九", "9");
+        return str;
+    }
+
+    public static void main(String[] args) {
+        String str = "聊聊高并发（十七）解析java.util.concurrent各个组件（一） 了解sun.misc.Unsafe类 - ITer_ZC的专栏";
+        System.out.println(sort(str));
+    }
+
+}
