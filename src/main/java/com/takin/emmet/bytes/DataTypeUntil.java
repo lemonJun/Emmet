9cbcf5c515f54f8f067b76a7eb7d4d20f01f3ec3
lemonJun <506526593@qq.com>
2017-06-02 17:30:09 +0800

    java 会把byte当成有符号数

--- src/main/java/com/takin/emmet/bytes/DataTypeUntil.java
+++ src/main/java/com/takin/emmet/bytes/DataTypeUntil.java
@@ -2,6 +2,11 @@ package com.takin.emmet.bytes;
 
 public class DataTypeUntil {
 
+    public static int byteToInt(byte b) {
+        //Java 总是把 byte 当做有符处理；我们可以通过将其和 0xFF 进行二进制与得到它的无符值  
+        return b & 0xFF;
+    }
+
     /**
      * 2字节 bytes 转换为Short,Endian为小端
      * **/
