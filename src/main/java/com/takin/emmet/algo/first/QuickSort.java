9f4ed86f5a079790ec96ab307ef5b071aa01b837
lemonJun <506526593@qq.com>
2017-06-04 12:15:00 +0800

    增加基础算法实现

---
+++ src/main/java/com/takin/emmet/algo/first/QuickSort.java
@@ -0,0 +1,33 @@
+package com.takin.emmet.algo.first;
+
+public class QuickSort {
+
+    public static void sort(int[] data, int left, int right) {
+        if (left < right) {
+            int q = partition(data, left, right);
+            sort(data, left, q - 1);
+            sort(data, q + 1, right);
+        }
+    }
+
+    /**
+     * 分区的过程 把数据按进行交换  同时返回对照数据所在的位置
+     * @param data
+     * @param left
+     * @param right
+     * @return
+     */
+    public static int partition(int[] data, int left, int right) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[] data = { 6, 3, 2, 5, 16, 23, 12, 8, 10, 9 };
+        //        int[] retVal = sort(data);
+        //        for (int i : retVal) {
+        //            System.out.print(i + " ");
+        //        }
+    }
+
+}
